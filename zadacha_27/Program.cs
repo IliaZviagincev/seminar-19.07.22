b1c9504148203379efc3f1b74cc030935bb43c25
Ilia Zviagincev <akinoro19@gmail.com>
2022-07-21 22:47:56 +0300

    подготовка к выгрузке

---
+++ zadacha_27/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int getSum(int x)
+{
+    int result = 0;
+    int x_temp = x;
+    int x_len = 0;
+    for (int k = 1; x_temp > 0; k++)
+    {
+        x_temp /= 10;
+        x_len = k;
+    }
+
+    int[] temp = new int[x_len];
+
+    for (int i = 0; i < x_len; i++)
+    {
+        temp[i] = x % 10;
+        x /= 10;
+        result += temp[i];
+    }
+
+    return result;
+}
+
+Console.Write("Введите число:   ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine($"Сумма чисел {number} равна {getSum(number)} ");
---
+++ zadacha_29/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+void arrayPrint(string[] arr)
+{
+    int[] temp = new int[arr.Length];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        temp[i] = int.Parse(arr[i]);
+        Console.Write($"{temp[i]}   ");
+    }
+}
+Console.Write("Введите через пробел числа для заполнения массива: ");
+string[] array = Console.ReadLine().Split();
+arrayPrint(array);
